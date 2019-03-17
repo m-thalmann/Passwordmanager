4330eb6b92bc34b3d23512dbfd32dac041e98c09
root <fanat.barca15@gmail.com>
2019-03-17 13:21:41 +0100

    added button which checks passwords with haveibeenpwned api and prints in console if it found the password

--- src/app/page-passwords/page-passwords.component.ts
+++ src/app/page-passwords/page-passwords.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PasswordsService } from '../passwords.service';
 import { Password } from '../api.service';
+import * as sha1 from 'sha1';
+
+
 
 @Component({
   selector: 'app-page-passwords',
@@ -11,13 +15,17 @@ export class PagePasswordsComponent implements OnInit, OnDestroy{
   private _pwords = null;
   private search = null;
 
+
   get pwords(){
     return PasswordsService.search(this._pwords, this.search);
   }
 
-  private pword_subscription = null;
 
-  constructor(private passwords: PasswordsService) {
+
+  private pword_subscription = null;
+  private check_subscription = null;
+
+  constructor(private passwords: PasswordsService, private http: HttpClient) {
   }
 
   ngOnInit() {
@@ -26,15 +34,35 @@ export class PagePasswordsComponent implements OnInit, OnDestroy{
 
       this.pword_subscription = this.passwords.get().subscribe((data: Password[]) => {
         this._pwords = data;
-      })
+      });
     });
   }
 
   ngOnDestroy() {
     this.pword_subscription.unsubscribe();
+    this.check_subscription.unsubscribe();
   }
 
-  searched(search: string){
+  searched(search: string) {
     this.search = search;
   }
+  async check() {
+
+    this._pwords.map(pword => {
+      let encrypted = pword['data']['password'];
+      encrypted = sha1(encrypted);
+      let shortenc = encrypted.substring(0, 5);
+      console.log(encrypted);
+      this.check_subscription = this.http.get('https://api.pwnedpasswords.com/range/' + shortenc, {responseType: 'text'}).subscribe(data => {
+        let arr = data.split('\n');
+        arr.forEach(hash => {
+          console.log(encrypted.toUpperCase());
+          if ( encrypted.toUpperCase() === shortenc.toUpperCase() + hash.substring(0, hash.indexOf(':'))) {
+            console.log('found password ' + hash.substring(hash.indexOf(':')) + ' times');
+          }
+        });
+      });
+
+  });
+}
 }
