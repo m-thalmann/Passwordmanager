522f9d6d8c959f2c4c6a0b793485e8b88ed7f735
AaronTarga <aaron.targa@tfobz.net>
2019-03-18 16:00:26 +0100

    small changes

--- src/app/check.service.ts
+++ src/app/check.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as sha1 from 'sha1';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   _checked: any[] = [];
 
@@ -25,5 +27,25 @@ export class CheckService {
       }
     });
     return ret;
+  }
+
+  async check(pwords) {
+    let index = 0;
+    let res = [];
+    pwords.map(async pword => {
+      let encrypted = pword['data']['password'];
+      encrypted = sha1(encrypted);
+      let shortenc = encrypted.substring(0, 5);
+      let data = await this.http.get('https://api.pwnedpasswords.com/range/' + shortenc, {responseType: 'text'}).toPromise();
+
+      let arr = data.split('\n');
+      arr.forEach(hash => {
+        if ( encrypted.toUpperCase() === shortenc.toUpperCase() + hash.substring(0, hash.indexOf(':'))) {
+          res.push({"index": index, "times": hash.substring(hash.indexOf(':')+1,hash.indexOf("\r"))})
+        }
+      });
+      index ++;
+      this.checked = res;
+    });
   }
 }
--- src/app/page-passwords/page-passwords.component.ts
+++ src/app/page-passwords/page-passwords.component.ts
@@ -1,11 +1,7 @@
 import { CheckService } from './../check.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PasswordsService } from '../passwords.service';
 import { Password } from '../api.service';
-import * as sha1 from 'sha1';
-
-
 
 @Component({
   selector: 'app-page-passwords',
@@ -21,11 +17,9 @@ export class PagePasswordsComponent implements OnInit, OnDestroy{
     return PasswordsService.search(this._pwords, this.search);
   }
 
-
-
   private pword_subscription = null;
 
-  constructor(private passwords: PasswordsService, private http: HttpClient, private checked: CheckService) {
+  constructor(private passwords: PasswordsService,  private check: CheckService) {
   }
 
   ngOnInit() {
@@ -34,14 +28,12 @@ export class PagePasswordsComponent implements OnInit, OnDestroy{
 
       this.pword_subscription = this.passwords.get().subscribe((data: Password[]) => {
         this._pwords = data;
+        this.check.check(data);
       });
 
-      this.check();
+      this.check.check(this._pwords);
 
     });
-
-
-
   }
 
   ngOnDestroy() {
@@ -52,23 +44,5 @@ export class PagePasswordsComponent implements OnInit, OnDestroy{
     this.search = search;
   }
 
-  async check() {
-    let index = 0;
-    let res = [];
-    this._pwords.map(async pword => {
-      let encrypted = pword['data']['password'];
-      encrypted = sha1(encrypted);
-      let shortenc = encrypted.substring(0, 5);
-      let data = await this.http.get('https://api.pwnedpasswords.com/range/' + shortenc, {responseType: 'text'}).toPromise();
 
-      let arr = data.split('\n');
-      arr.forEach(hash => {
-        if ( encrypted.toUpperCase() === shortenc.toUpperCase() + hash.substring(0, hash.indexOf(':'))) {
-          res.push({"index": index, "times": hash.substring(hash.indexOf(':')+1,hash.indexOf("\r"))})
-        }
-      });
-      index ++;
-      this.checked.checked = res;
-    });
-  }
 }
